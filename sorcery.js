75276a1cb50004d8f6998fb1d5dc6dde008af23b
MichaelMondou <themikamozart33@live.fr>
2017-01-14 01:25:23 +0100

    Initial commit

---
+++ sorcery.js
@@ -0,0 +1,35 @@
+$( function() {
+	var buttons = $(".section button");
+	var status = $("#status");
+	
+	buttons.click( function() {
+		//...
+	} );
+	
+	
+	
+	function gotoSection(key) {
+		//...
+	}
+	
+	function getLife() {
+		//...
+	}
+	
+	function setLife(v) {
+		//...
+	}
+	
+	function loseOneLife() {
+		//...
+	}
+	
+	function startGame() {
+		//...
+	}
+	
+	function endGame() {
+		//...
+	}
+	
+} );
