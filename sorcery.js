7ea0421eb8feb897f1ba2d54b5d8007bfba7a2a1
MichaelMondou <themikamozart33@live.fr>
2017-01-14 01:27:32 +0100

    v1.0

--- sorcery.js
+++ sorcery.js
@@ -1,35 +1,58 @@
-$( function() {
-	var buttons = $(".section button");
-	var status = $("#status");
-	
-	buttons.click( function() {
-		//...
-	} );
-	
-	
-	
-	function gotoSection(key) {
-		//...
-	}
-	
-	function getLife() {
-		//...
-	}
-	
-	function setLife(v) {
-		//...
-	}
-	
-	function loseOneLife() {
-		//...
-	}
-	
-	function startGame() {
-		//...
-	}
-	
-	function endGame() {
-		//...
-	}
-	
-} );
+$(function () {
+    var buttons = $(".section button");
+    var status = $("#status");
+
+    var functions = {
+        reset: function resetGame() {
+            setLife('--');
+        },
+        start: function startGame() {
+            setLife('3');
+        },
+        hit: function loseOneLife() {
+            var nb_lifes = getLife();
+
+            if (nb_lifes - 1 == 0) {
+                endGame();
+            }
+
+            setLife(nb_lifes - 1);
+        }
+    };
+
+    $(".section").hide();
+
+    buttons.click(function () {
+        $(this).parents("div:first").hide();
+        gotoSection($(this).attr("go"));
+    });
+
+    gotoSection('intro');
+
+    function gotoSection(key) {
+        var section = $("#" + key);
+        section.show();
+        executeAction(section);
+    }
+
+    function executeAction(section) {
+        var action = section.find("action").attr('name');
+        if (action != undefined) {
+            functions[action]();
+        }
+    }
+
+    function getLife() {
+        return parseInt($(".life .value").text());
+    }
+
+    function setLife(v) {
+        $(".life .value").text(v);
+    }
+
+    function endGame() {
+        $('.section').hide();
+        gotoSection('death');
+    }
+
+});
